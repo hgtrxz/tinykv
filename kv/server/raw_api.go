848ccd1a08067e33011f26a4dd58a0b0ec209c1f
shize.wang <3043719361@qq.com>
2022-07-15 16:16:34 +0800

    pass proj1

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -11,20 +12,47 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+	reader, err := server.storage.Reader(nil)
+	if err != nil {
+		return nil, err
+	}
+	val, err := reader.GetCF(req.GetCf(), req.GetKey())
+	if err != nil {
+		return nil, err
+	}
+	resp := &kvrpcpb.RawGetResponse{Value: val}
+	if val == nil {
+		resp.NotFound = true
+	}
+	return resp, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+	put := storage.Put{
+		Key:   req.GetKey(),
+		Value: req.GetValue(),
+		Cf:    req.GetCf(),
+	}
+	if err := server.storage.Write(nil, []storage.Modify{{Data: put}}); err != nil {
+		return nil, err
+	}
+	return &kvrpcpb.RawPutResponse{}, nil
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
+	del := storage.Delete{
+		Key: req.GetKey(),
+		Cf:  req.GetCf(),
+	}
+	if err := server.storage.Write(nil, []storage.Modify{{Data: del}}); err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
@@ -32,5 +60,24 @@ func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+	reader, err := server.storage.Reader(nil)
+	if err != nil {
+		return nil, err
+	}
+	iterator := reader.IterCF(req.GetCf())
+	iterator.Seek(req.StartKey)
+	kvs := make([]*kvrpcpb.KvPair, 0)
+	for i := 0; i < int(req.GetLimit()); i++ {
+		if !iterator.Valid() {
+			break
+		}
+		k := iterator.Item().Key()
+		v, _ := iterator.Item().Value()
+		kvs = append(kvs, &kvrpcpb.KvPair{
+			Key:   k,
+			Value: v,
+		})
+		iterator.Next()
+	}
+	return &kvrpcpb.RawScanResponse{Kvs: kvs}, nil
 }
